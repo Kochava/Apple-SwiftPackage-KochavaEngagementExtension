17b53e6c507e86a32ff4914b3c1a036566fa293a
John Bushnell <jbushnell@kochava.com>
2021-11-10 17:02:22 -0800

    5.1.0

--- KochavaEngagementExtension.xcframework/macos-arm64_x86_64/KochavaEngagementExtension.framework/Versions/A/Headers/UNNotificationServiceExtension+KochavaEngagementExtension.h
+++ KochavaEngagementExtension.xcframework/macos-arm64_x86_64/KochavaEngagementExtension.framework/Versions/A/Headers/UNNotificationServiceExtension+KochavaEngagementExtension.h
@@ -27,11 +27,11 @@
  
  @brief Makes changes to the notification as part of Kochava's Engagement support.
 
- @discussion Call this method to modify the UNNotificationContent object that was delivered with the notification.  At some point during your implementation and following the call to the completionHandler, execute the contentHandler block and pass it the modified content.
+ @discussion Call this method to modify the UNNotificationContent object that was delivered with the notification.  At some point during your implementation and following the call to the completionHandler, execute the contentHandler closure and pass it the modified content.
  
  Through this method you can automatically modify various content from the original request.  You might customize the content for the current user or replace it altogether.  You can use this method to automatically download images or movies and add them as attachments to the content.  You may also automatically modify the alert text using token substitution.
  
- This method has a limited amount of time (no more than 30 seconds) to modify the content and execute the completionHandler block.  If it does not execute that block in a timely manner, the system calls your extension’s serviceExtensionTimeWillExpire() method to give you one last chance to execute the contentHandler block.  If you do not, the system presents the notification’s original content to the user.
+ This method has a limited amount of time (no more than 30 seconds) to modify the content and execute the completionHandler closure.  If it does not execute that closure in a timely manner, the system calls your extension’s serviceExtensionTimeWillExpire() method to give you one last chance to execute the contentHandler closure.  If you do not, the system presents the notification’s original content to the user.
  
  @param request The original notification request.
  
@@ -39,7 +39,7 @@
  
  @param tokenReplacementDictionary An optional dictionary ([String: String]?) which can be used to define token keys with their respective substitution values.  An example token key might be "name", where the associated substitution value might be "Leroy".
  
- @param completionHandler The block to execute when completed with any asynchronously gathered content.  This block has no return value and passes the following parameter:
+ @param completionHandler The closure to execute when completed with any asynchronously gathered content.  This closure has no return value and passes the following parameter:
  asynchronousNotificationAttachmentArray
  An array of UNNotificationAttachment(s) which were gathered asynchronously to be appended to the notification content.
  */
